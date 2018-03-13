ef46d0dc8cc1617ebef451ebe3ca34b039817ba9
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-13 20:47:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if number % 15 == 0
-    "Fizz"
-  elsif number % 3 == 0
-    "Buzz"
-  elsif number % 5 == 0
     "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
 
   end
 end
